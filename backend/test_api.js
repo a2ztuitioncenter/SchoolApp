254082e1f1a26be2c34f568bb13c4d7e80593a5e
muslimuddin <uddinemuslim@gmail.com>
2026-03-31 21:59:24 +0530

    delete temo files

--- backend/test_api.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-http.get('http://localhost:3000/api/admin/notifications', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => {
-    console.log('Status:', res.statusCode);
-    console.log('Body:', data);
-  });
-}).on('error', err => console.error(err));
